22aaf0c44bf16ab2db885bd6dcafcf54d87b5ee9
Justin Hoffman <justinhoffman193@gmail.com>
2021-08-30 01:34:14 -0500

    book add

---
+++ server/routes/api/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
+
+
+
+// const path = require('path');
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+// // serve up react front-end in production
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// });
+
